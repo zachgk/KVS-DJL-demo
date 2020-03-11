0bc0336a6dd71bcb4a01d26bde8a266e9392bfcf
Zach Kimberg <kimbergz@amazon.com>
2020-03-11 14:28:03 -0700

    Improve logging of outputs with person detection

--- src/main/java/org/example/DjlImageVisitor.java
+++ src/main/java/org/example/DjlImageVisitor.java
@@ -3,6 +3,7 @@ package org.example;
 import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
+import ai.djl.modality.Classifications.Classification;
 import ai.djl.modality.cv.DetectedObjects;
 import ai.djl.mxnet.zoo.MxModelZoo;
 import ai.djl.repository.zoo.ModelNotFoundException;
@@ -15,6 +16,7 @@ import com.amazonaws.kinesisvideo.parser.utilities.MkvTrackMetadata;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DjlImageVisitor extends H264FrameDecoder {
 
@@ -31,8 +33,11 @@ public class DjlImageVisitor extends H264FrameDecoder {
         BufferedImage bufferedImage = decodeH264Frame(frame, trackMetadata);
 
         try {
-            Classifications prediction = predictor.predict(bufferedImage);
-            System.out.println("Found " + prediction.items().size() + " objects");
+            DetectedObjects prediction = predictor.predict(bufferedImage);
+            String classStr = prediction.items().stream().map(Classification::getClassName).collect(
+                Collectors.joining());
+            System.out.println("Found objects: " + classStr);
+            boolean hasPerson = prediction.items().stream().anyMatch(c -> "person".equals(c.getClassName()) && c.getProbability() > 0.5);
         } catch (TranslateException e) {
             throw new FrameProcessException("Failed to predict", e);
         }
