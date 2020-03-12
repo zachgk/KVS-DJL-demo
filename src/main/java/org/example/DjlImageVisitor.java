b70b6f6f00c2a1e6f4ca22b25087255f401b18b5
Zach Kimberg <kimbergz@amazon.com>
2020-03-12 13:45:15 -0700

    Add ImageVisualization output

--- src/main/java/org/example/DjlImageVisitor.java
+++ src/main/java/org/example/DjlImageVisitor.java
@@ -2,9 +2,9 @@ package org.example;
 
 import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
-import ai.djl.modality.Classifications;
 import ai.djl.modality.Classifications.Classification;
 import ai.djl.modality.cv.DetectedObjects;
+import ai.djl.modality.cv.ImageVisualization;
 import ai.djl.mxnet.zoo.MxModelZoo;
 import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.translate.TranslateException;
@@ -14,16 +14,21 @@ import com.amazonaws.kinesisvideo.parser.utilities.FragmentMetadata;
 import com.amazonaws.kinesisvideo.parser.utilities.H264FrameDecoder;
 import com.amazonaws.kinesisvideo.parser.utilities.MkvTrackMetadata;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
 
 public class DjlImageVisitor extends H264FrameDecoder {
 
     Predictor<BufferedImage, DetectedObjects> predictor;
+    int counter;
 
     public DjlImageVisitor() throws IOException, ModelNotFoundException, MalformedModelException {
         predictor = MxModelZoo.SSD.loadModel().newPredictor();
+        counter = 0;
     }
 
     @Override
@@ -35,10 +40,15 @@ public class DjlImageVisitor extends H264FrameDecoder {
         try {
             DetectedObjects prediction = predictor.predict(bufferedImage);
             String classStr = prediction.items().stream().map(Classification::getClassName).collect(
-                Collectors.joining());
+                Collectors.joining(", "));
             System.out.println("Found objects: " + classStr);
             boolean hasPerson = prediction.items().stream().anyMatch(c -> "person".equals(c.getClassName()) && c.getProbability() > 0.5);
-        } catch (TranslateException e) {
+
+            ImageVisualization.drawBoundingBoxes(bufferedImage, prediction);
+            File outputFile = Paths.get("out/image-" + counter + ".png").toFile();
+            ImageIO.write(bufferedImage, "png", outputFile);
+            counter++;
+        } catch (TranslateException | IOException e) {
             throw new FrameProcessException("Failed to predict", e);
         }
     }
